da31ec371c0e659b640a5dc953680b4fb3944818
Vaerish <31716914+Vaerish@users.noreply.github.com>
2021-07-06 16:14:43 -0500

    Add files via upload

---
+++ hw7_replace_fix_obvious_decimal_errors_AIT202.R
@@ -0,0 +1,24 @@
+df =  read.csv("C:\\Users\\shado_000\\datamining\\data-mining-data\\5402_dataset.csv")
+
+
+for (i in 1:dim(df)[1]) {
+	if( !is.na(df$AIT202[i]) && df$AIT202[i] > 10)
+	{
+		while((df$AIT202[i] > 10))
+		{
+			df$AIT202[i] <- df$AIT202[i] /10
+		}
+	}
+	if( !is.na(df$AIT202[i]) && df$AIT202[i] < 8)
+	{
+		while((df$AIT202[i] < 8))
+		{
+			df$AIT202[i] <- df$AIT202[i] *10
+		}
+	}
+
+}
+
+
+
+write.csv(df,"C:\\Users\\shado_000\\datamining\\data-mining-data\\5402_dataset2.csv", row.names = FALSE)
